57900fb552eadb32844d9320e334f7a12644b241
Samra Moinuddin <160382104+sheikhsamra@users.noreply.github.com>
2025-06-24 18:53:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Error! Please anter a valid password.");
+alert("Welcome to JS Land \n Happy Coding");
+alert("Welcome to JS Land");
+alert("Happy Coding");
+alert("Hello... I can run JS through my web browser's console");
+console.log("Hello... I can run JS through my web browser's console");
